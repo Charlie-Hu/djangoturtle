2cbfd2b89854b3100abe1b1bef5219b1fb173b82
Charlie-Hu <23217014@student.uwa.edu.au>
2022-08-26 19:51:12 +0800

    day8 requirement

--- djangoturtle/settings.py
+++ djangoturtle/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'team_turtle',
-        'USER': 'Charlie',
-        'PASSWORD': 'Charlie028',
+        'USER': 'root',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
