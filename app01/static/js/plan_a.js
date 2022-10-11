41e6bccd91c9b43fe5326515effaba043835c3e2
Charlie-Hu <23217014@student.uwa.edu.au>
2022-10-11 18:03:30 +0800

    fix plan bug and beautify

--- app01/static/js/plan_a.js
+++ app01/static/js/plan_a.js
@@ -42,7 +42,7 @@ function time_maker() {
     //let str = plan1.innerHTML;
     let tpl = "";
     for (let i = 0; i < n; i++) {
-        tpl += `<div class="inpIte"><span>times${i + 1}: </span><input type="time" name=num_time${i}></div>`
+        tpl += `<div class="inpIte"><span>Scheduled times ${i + 1}: </span><input type="time" name=num_time${i}"></div>`
     }
     dync.innerHTML = tpl
 }
--- app01/views.py
+++ app01/views.py
@@ -86,9 +86,9 @@ def plan(request):
     for i in range(0, int(times)):
         per_num_time = request.POST.get('num_time{num}'.format(num=i))
         num_time.append(per_num_time)
-
     em_email = request.POST.get("email")
-    if len(med_name) < 1 or len(dosage) < 1 or len(times) < 1 or len(num_time) < 1:
+    print(len(num_time))
+    if med_name is None or dosage is None or float(dosage) < 0 or times is None or num_time[0] is None or em_email is None:
         return render(request, "plan.html", {"state1": "input detail can not be empty"})
     new_time = ',  '.join(num_time)
     Userplan.objects.create(name=username, medicine_name=med_name, dosage=dosage, times=times, num_time=new_time,
